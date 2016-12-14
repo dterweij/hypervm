40f4a2f4e64ca6174470c6eb127a515eb4d33cbd
dterweij <danny@terweij.nl>
2016-12-14 17:56:34 +0100

    Very weird PHP errors :(

--- hypervm/bin/common/misc/fixlighty.php
+++ hypervm/bin/common/misc/fixlighty.php
@@ -28,7 +28,7 @@ $list = parse_opt($argv);
 if (!isset($list['default-port']) && !lxfile_exists("__path_slave_db")) {
 
 	initProgram('admin');
-	$gen = (isset($login->getObject('general')->portconfig_b));
+	$gen = ($login->getObject('general')->portconfig_b);
 	if ($gen) {
 		if ($gen->isOn('nonsslportdisable_flag')) {
 			$nonsslhash = "";
